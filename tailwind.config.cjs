164595dc0df4237748b60a3e87812c1690dd63a2
WuTangLan1 <finnmassari16@gmail.com>
2025-01-25 21:32:47 +0200

    finally fixed tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+// tailwind.config.cjs
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  // postcss.config.cjs
+  module.exports = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  }
